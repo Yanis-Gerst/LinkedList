0341992635fca9363c3fc36b4a9fddb8f315c243
Ikims <107883257+Yanis-Gerst@users.noreply.github.com>
2022-10-22 19:45:05 +0200

    Initial Commit

---
+++ Data-Structure/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Data-Structure
+//
+//  Created by Yanis Gerst on 22/10/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
